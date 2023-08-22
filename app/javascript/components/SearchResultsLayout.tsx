3b8e01ed8c2c4d691207e0f7a428ab542dc4cb46
Steven Barragan Naranjo <me@steven.mx>
2023-08-22 15:37:35 -0700

    SRCH-4455 Internationalize the new serp

--- app/javascript/components/SearchResultsLayout.tsx
+++ app/javascript/components/SearchResultsLayout.tsx
@@ -9,6 +9,7 @@ import { SearchBar } from './SearchBar/SearchBar';
 import { Results } from './Results/Results';
 import { Footer } from './Footer/Footer';
 import { Identifier } from './Identifier/Identifier';
+import { LanguageContext } from '../contexts/LanguageContext';
 
 interface SearchResultsLayoutProps {
   resultsData?: {
@@ -124,7 +125,7 @@ const SearchResultsLayout = ({ resultsData, additionalResults, vertical, params 
   i18n.locale = language;
 
   return (
-    <>
+    <LanguageContext.Provider value={i18n}>
       <Header 
         title={getAffiliateTitle()}
         isBasic={isBasicHeader()} 
@@ -132,10 +133,7 @@ const SearchResultsLayout = ({ resultsData, additionalResults, vertical, params 
      
       <div className="usa-section serp-result-wrapper">
         <Facets />
-        <SearchBar 
-          query={params.query}
-          locale={i18n}
-        />
+        <SearchBar query={params.query} />
         {/* This ternary is needed to handle the case when Bing pagination leads to a page with no results */}
         {resultsData ? (
           <Results 
@@ -145,20 +143,18 @@ const SearchResultsLayout = ({ resultsData, additionalResults, vertical, params 
             query={params.query}
             unboundedResults={resultsData.unboundedResults}
             additionalResults={additionalResults}
-            locale={i18n}
           />) : params.query ? (
           <Results 
             vertical={vertical}
             totalPages={null}
             query={params.query}
             unboundedResults={true}
-            locale={i18n}
           />) : <></>}
       </div>
 
       <Footer />
       <Identifier />
-    </>
+    </LanguageContext.Provider>
   );
 };
 
